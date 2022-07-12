606377085e67780efde7a6800289305063a4d2e5
Donggyu Han <lucky5632@naver.com>
2022-07-12 23:09:25 +0900

    ADD : Message RestController

---
+++ src/main/java/Study/ApiStudy/controller/MessageController.java
@@ -0,0 +1,10 @@
+package Study.ApiStudy.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MessageController {
+
+}
